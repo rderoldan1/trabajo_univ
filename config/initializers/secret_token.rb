f1c22b11f04b8c06ea71b2b65dd7869275e74377
Ruben Espinosa <rderoldan1@gmail.com>
2013-03-27 21:24:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrabajoUniv::Application.config.secret_token = 'e8b7d967e38498991c8f5347023dc63bd8dbec480dc73e17c95db2bbdd9f76249e4636be9643db54a6af4b43893271d91e30274314ed5f84ae60b8209cebf99f'
